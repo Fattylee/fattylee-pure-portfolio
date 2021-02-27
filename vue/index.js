d45cc97450f6fa9776351769a82f8a699907313b
fatai balogun <fattylee.remod@gmail.com>
2021-02-27 22:42:07 +0100

    learn few concept in vue: key modifiers, modifiers key, events, bind etc

--- vue/index.js
+++ vue/index.js
@@ -2,6 +2,27 @@ new Vue({
   el:'#app',
   data:{
     name:'abu adnaan',
-    age:33
+    age:33,
+    result:'',
+  },
+  methods:{
+    sum(factor){
+      return this.age+factor;
+    },
+    alert(){
+      console.log('see me');
+      alert(7);
+    },
+    keyPressed(tap){
+const {value}=tap.target;
+      console.log('Some keys...',value)
+      this.result=value;
+    },
+    clickMe(event){
+      console.log('Button clicked!!')
+    },
+   submitForm(e){
+     console.log('Hello world!')
+   } 
   }
 })
